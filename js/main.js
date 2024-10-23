a6aec77f448270a2c8996eb777f209bfc8e02ddb
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-23 22:11:12 +0200

    Add js function

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
 const inputNameEl = document.getElementById("name-input");
 const inputKmEl = document.getElementById("Km-input");
 const inputAgeEl = document.getElementById("age-input");
-const btnTicketGeneratorEl = document.getElementById("btn-generator-ticket");
+const btnTicketEl = document.getElementById("btn-ticket");
 const ticketDeletedEl = document.getElementById("delete-ticket");
 const ticketUserInfo = document.getElementById("info-ticket-user");
+const outputCardEl = document.getElementById("output-card");
+const priceCostEl = document.getElementById("price-cost");
+const typeTicketEl = document.getElementById("type-ticket");
+const passengerNameEl = document.getElementById("passenger-name");
 
 console.log(inputNameEl);
 console.log(inputKmEl);
 console.log(inputAgeEl);
 
-btnTicketGeneratorEl.addEventListener("click", () => {
-  const fullName = inputNameEl.value;
+const kmCost = 0.21;
 
-  if (fullName.length < 3) {
-    console.log("Errore");
-  }
+const discountForUnder18 = 20;
+const discountForOver65 = 40;
+
+ticketUserInfo.addEventListener("Submit", (event) => {
+  event.preventDefault();
+
+  const fullName = inputNameEl.value;
+  const AgeInput = inputAgeEl.value;
+  const priceTicket = kmCost * inputKmEl;
 });
