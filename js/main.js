90ff784a1dcf4942c44cd769932cd37145df79c5
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-23 16:33:01 +0200

    Add km cost

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 const inputNameEl = document.getElementById("name-input");
 const inputKmEl = document.getElementById("Km-input");
 const inputAgeEl = document.getElementById("age-input");
-const ticketGeneratorEl = document.getElementById("generator-ticket");
+const btnTicketGeneratorEl = document.getElementById("btn-generator-ticket");
 const ticketDeletedEl = document.getElementById("delete-ticket");
+const ticketUserInfo = document.getElementById("info-ticket-user");
 
 console.log(inputNameEl);
 console.log(inputKmEl);
 console.log(inputAgeEl);
 
 const kmCost = 0.21;
-
-ticketGeneratorEl.addEventListener("click", () => {
-  console.log(inputAgeEl.value);
-});
+let NumKm = inputKmEl;
+const priceTicket = NumKm * kmCost;
