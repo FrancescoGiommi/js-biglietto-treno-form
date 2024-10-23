641630dedee7725539bb4c4ea8d0c8c52eb4ef0e
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-23 14:24:08 +0200

    Add form and js id element

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const inputNameEl = document.getElementById("name-input");
+const inputKmEl = document.getElementById("Km-input");
+const inputAgeEl = document.getElementById("age-input");
+const ticketGeneratorEl = document.getElementById("generator-ticket");
+const ticketDeletedEl = document.getElementById("delete-ticket");
+
+console.log(inputNameEl);
+console.log(inputKmEl);
+console.log(inputAgeEl);
