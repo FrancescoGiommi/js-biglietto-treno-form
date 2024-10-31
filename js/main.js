be3da7cd0cc4f3d5410039e04cb42cbb1d62f00c
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-31 15:18:48 +0100

    Add submint with form

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ const under18El = document.getElementById("under-18");
 const over65El = document.getElementById("over-65");
 const adultEl = document.getElementById("adult");
 const ticketBtnEl = document.getElementById("btn-ticket");
-const outputTicketEl = document.getElementById("output-ticket");
+
 const ticketFormEl = document.getElementById("ticket-user-form");
 const deleteTicketEl = document.getElementById("delete-ticket");
 
@@ -44,6 +44,7 @@ const ticketRateEl = document.getElementById("rate-ticket");
 const carriageEl = document.getElementById("carriage");
 const cpCodeEl = document.getElementById("cp-code");
 const ticketPriceEl = document.getElementById("ticket-price");
+const outputTicketEl = document.getElementById("output-ticket");
 
 ticketFormEl.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -51,23 +52,73 @@ ticketFormEl.addEventListener("submit", (event) => {
   const userName = inputNameEl.value;
   const distanceKm = inputKmEl.value;
   const ageUser = inputAgeEl.value;
+  const under18 = under18El.value;
+  const adult = adultEl.value;
+  const over65 = over65El.value;
   console.log(userName, distanceKm + " Km", ageUser + " anni");
 
-  const kmCost = 0.21;
-
-  let discountPrice = 0;
-
-  if (ageUser == "under-18") {
-    const discountUnder18 = 20;
-    discountPrice = (priceTicket * discountUnder18) % 100;
-  } else if (ageUser == "over-65") {
-    const discountOver65 = 40;
-    discountPrice = (priceTicket - discountOver65) % 100;
+  const kmPrice = 0.21;
+  const priceTicket = (distanceKm * kmPrice).toFixed(2);
+  let discountEur;
+  let discountPerc;
+  let rateTicket = "Biglietto Standard";
+  discountEur = (priceTicket - discountPerc) % 100;
+  const finalPrice = priceTicket - discountEur;
+  if (ageUser == under18) {
+    discountPerc = 20;
+    rateTicket = "Biglietto Under 18";
+  } else if (ageUser == over65) {
+    discountPerc = 40;
+    rateTicket = "Biglietto Over65";
   }
-  const priceTicket = kmCost * distanceKm;
 
-  console.log("prezzo scontato " + discountPrice);
-  console.log("prezzo del biglietto " + priceTicket);
-  console.log("prezzo del biglietto " + discountUnder18);
-  console.log("prezzo del biglietto " + discountOver65);
+  let outputTicket = "";
+
+  const generateTicket = `
+<div class="col-12 ">
+            <h3>DETTAGLIO PASSEGGERI</h3>
+          </div>
+          <div class="col-4">
+            <h3>NOME PASSEGGERO</h3>
+            <div class="card">
+              <div class="card-body ">
+                <span id="passenger-name">${userName}</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-2">
+            <h4>Offerta</h4>
+            <div class="card">
+              <div class="card-body">
+                <span id="rate-ticket">${rateTicket}</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-2">
+            <h4>Carrozza</h4>
+            <div class="card">
+              <div class="card-body">
+                <span id="carriage"></span>
+              </div>
+            </div>
+          </div>
+          <div class="col-2">
+            <h4>Codice CP</h4>
+            <div class="card">
+              <div class="card-body">
+                <span id="cp-code"></span>
+              </div>
+            </div>
+          </div>
+          <div class="col-2">
+            <h4>Costo biglietto</h4>
+            <div class="card">
+              <div class="card-body">
+                <span id="ticket-price">€ ${finalPrice}</span>
+              </div>
+            </div>
+          </div>
+`;
+  outputTicket += generateTicket;
+  outputTicketEl.innerHTML = outputTicket;
 });
